7fe61df6e336f11ebc49bec1712d72c0669fb5b2
Nuriza <nuriza.sk@gmail.com>
2013-12-09 17:38:54 +0300

    fixes #4 ishtei elek

--- server/src/main/java/hello/Application.java
+++ server/src/main/java/hello/Application.java
@@ -1,5 +1,6 @@
 package hello;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
